16d95a7a9711032e830fa5f1ac3050eca4d32010
dyyeon <dohy3141@naver.com>
2025-07-02 12:18:52 +0900

    3장 중간테스트 AllPass_4(수정/조회기능완성)

--- src/main/java/com/dh/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/dh/book/springboot/service/posts/PostsService.java
@@ -2,7 +2,9 @@ package com.dh.book.springboot.service.posts;
 
 import com.dh.book.springboot.domain.posts.Posts;
 import com.dh.book.springboot.domain.posts.PostsRepository;
+import com.dh.book.springboot.web.dto.PostsResponseDto;
 import com.dh.book.springboot.web.dto.PostsSaveRequestDto;
+import com.dh.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,5 +19,20 @@ public class PostsService {
     public Long save(PostsSaveRequestDto requestDto) {
         Posts entity = requestDto.toEntity();
         return postsRepository.save(entity).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById (Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
+
+        return new PostsResponseDto(entity);
     }
 }
--- src/test/java/com/dh/book/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/dh/book/springboot/web/PostsApiControllerTest.java
@@ -3,12 +3,15 @@ package com.dh.book.springboot.web;
 import com.dh.book.springboot.domain.posts.Posts;
 import com.dh.book.springboot.domain.posts.PostsRepository;
 import com.dh.book.springboot.web.dto.PostsSaveRequestDto;
+import com.dh.book.springboot.web.dto.PostsUpdateRequestDto;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -55,5 +58,32 @@ public class PostsApiControllerTest {
         Posts post = postsRepository.findAll().get(0);
         assert(post.getTitle().equals(title));
         assert(post.getContent().equals(content));
+    }
+
+    @Test
+    public void Posts_수정된다() throws Exception {
+        //given
+        Posts savedPosts = postsRepository.save(Posts.builder().title("title").content("content").author("author").build());
+
+        Long updateId = savedPosts.getId();
+        String expectedTitle = "title2";
+        String expectedContent = "content2";
+
+        PostsUpdateRequestDto requestDto = PostsUpdateRequestDto.builder().title(expectedTitle).content(expectedContent).build();
+
+        String url = "http://localhost:" + port + "/api/v1/posts/" + updateId;
+
+        HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
+
+        //when
+        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
+
+        //then
+        assert(responseEntity.getStatusCode() == HttpStatus.OK);
+        assert(responseEntity.getBody() > 0);
+
+        Posts post = postsRepository.findAll().get(0);
+        assert(post.getTitle().equals(expectedTitle));
+        assert(post.getContent().equals(expectedContent));
     }
 }
