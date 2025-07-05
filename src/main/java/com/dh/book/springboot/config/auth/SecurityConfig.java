365ff66ce425d935ff29e308446b3c05ae065a11
dyyeon <dohy3141@naver.com>
2025-07-05 17:04:05 +0900

    5장 중간테스트 AllPass_2(구글로그인연동)

---
+++ src/main/java/com/dh/book/springboot/config/auth/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.dh.book.springboot.config.auth;
+
+import com.dh.book.springboot.domain.user.UserRole;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@Configuration
+public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()))
+                .authorizeHttpRequests(auth -> auth
+                        .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                        .antMatchers("/api/v1/**").hasRole(UserRole.USER.name()) // "USER"
+                        .anyRequest().authenticated()
+                )
+                .logout(logout -> logout.logoutSuccessUrl("/"))
+                .oauth2Login(oauth2 -> oauth2
+                        .userInfoEndpoint(userInfo -> userInfo.userService(customOAuth2UserService))
+                );
+
+        return http.build();
+    }
+}
